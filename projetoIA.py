b4a524a4723ebdef3fa719d3a2852865ccf867db
Nicolas <gomes160107@gmail.com>
2025-05-24 17:11:14 -0300

    verifica_emails - corrigido
    Foi corrigido a estrutura de verificação!

--- projetoIA.py
+++ projetoIA.py
@@ -1,4 +1,4 @@
-# Importações
+
 import imaplib
 import email
 from email.header import decode_header
@@ -12,7 +12,7 @@ SMTP_SERVER = 'smtp.gmail.com'
 EMAIL_ACCOUNT = 'seu_email@gmail.com'
 EMAIL_PASSWORD = 'sua_senha_de_app'
 IMAP_PORT = 993 # Porta padrão para conexões IMAP seguras (SSL/TLS)
-SMPT_PORT = 587 #  Porta padrão para conexôes SMPT via STARTTLS (criptação)
+SMTP_PORT = 587 #  Porta padrão para conexôes SMPT via STARTTLS (criptação)
 
 def interpretar_mensagem(texto):
     """
@@ -35,7 +35,47 @@ def responder_email(destinatario, assunto, corpo):
     msg['To'] = destinatario
     msg['Subject'] = f"Re: {assunto}"
 
-    with smtplib.SMPT(SMTP_SERVER, SMPT_PORT) as server:
+    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
         server.starttls()
-        server.login(EMAIL_ACCOUNT, destinatario, msg.as_string())
+        server.login(EMAIL_ACCOUNT, EMAIL_PASSWORD)
+        server.sendmail(EMAIL_ACCOUNT, destinatario, msg.as_string())
         print(f"Resposta enviada para: {destinatario}")
+
+def verifica_emails():
+    """
+    Conecta na caixa de entrada e verifica novos e-mails.
+    """
+    with imaplib.IMAP4_SSL(IMAP_SERVER, IMAP_PORT) as mail:
+        mail.login(EMAIL_ACCOUNT, EMAIL_PASSWORD)
+        mail.select("inbox")
+
+        status, messages = mail.search(None, '(UNSEEN)')
+        if status != "OK":
+            print("Nenhumn e-mail novo.")
+            return
+        for num in messages[0].split():
+            status, data = mail.fetch(num, '(RFC822)')
+            if status != "OK":
+                continue
+            
+            msg = email.message_from_bytes(data[0][1])
+            assunto, encoding = decode_header(msg.get("from"))[1]
+            if isinstance(assunto, bytes):
+                assunto = assunto.decode(encoding if encoding else "utf-8")
+            remetente = email.utils.parseaddr(msg.get("From"))[1]
+
+            #pega o corpo do e-mail
+            if msg.is_multipart():
+                for part in msg.walk():
+                    if part.get_content_type() == "text/plain":
+                        corpo = part.get_payload(decode=True).decode()
+                        break
+            else:
+                corpo = msg.get_payload(decode=True).decode()
+
+            resposta = interpretar_mensagem(corpo)
+            responder_email(remetente, assunto, resposta)
+
+            # Marca como lido
+            mail.store(num, + 'FLAGS', '\\Seen')
+        
